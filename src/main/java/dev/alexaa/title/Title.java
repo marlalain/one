cf776ef28f3f6d0e3f04ac627f2269ac49592a0e
Alex <alex@alexaa.dev>
2022-10-23 17:58:43 -0300

    refactor(Title): moves `titles` to local variable

--- src/main/java/dev/alexaa/title/Title.java
+++ src/main/java/dev/alexaa/title/Title.java
@@ -1,27 +1,18 @@
 package dev.alexaa.title;
 
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Title {
 	public final String subject;
 
-	private final Collection<JobTitle> titles;
-
 	public Title(List<String> words) {
 		subject = words.get(words.size() - 1);
-
-		titles = Stream
-			.of(JobTitle.values())
-			.map(title -> title.withTitle(this))
-			.collect(Collectors.toList());
 	}
 
 	public String normalize() {
-		return titles
-			.stream()
+		return Arrays.stream(JobTitle.values())
+			.map(title -> title.withTitle(this))
 			.min(JobTitle::sort)
 			.orElseThrow(IllegalStateException::new)
 			.toString();
