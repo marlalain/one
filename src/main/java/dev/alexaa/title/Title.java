3711e7c16365b60ded7b665e863f8367d33a4156
Alex <alex@alexaa.dev>
2022-10-23 17:13:05 -0300

    refactor(Title): removes dead code

--- src/main/java/dev/alexaa/title/Title.java
+++ src/main/java/dev/alexaa/title/Title.java
@@ -1,38 +1,29 @@
 package dev.alexaa.title;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static dev.alexaa.title.JobTitle.*;
 
 public class Title {
-	public final String classification;
-	public final Collection<String> tail;
 	public final String subject;
 
-	private final Collection<JobTitle> titles = List.of(
-		SOFTWARE_ENGINEER,
-		ARCHITECT,
-		ACCOUNTANT,
-		QUANTITY_SURVEYOR
-	);
+	private final Collection<JobTitle> titles;
 
 	public Title(List<String> words) {
 		subject = words.get(words.size() - 1);
 
-		tail = words
-			.subList(0, words.size() - 1);
-
-		classification = StringUtils
-			.join(tail, " ");
+		titles = Stream
+			.of(SOFTWARE_ENGINEER, ARCHITECT, ACCOUNTANT, QUANTITY_SURVEYOR)
+			.map(title -> title.withTitle(this))
+			.collect(Collectors.toList());
 	}
 
 	public String normalize() {
 		return titles
 			.stream()
-			.map(jobTitle -> jobTitle.withTitle(this))
 			.min(JobTitle::sort)
 			.orElseThrow(IllegalStateException::new)
 			.toString();
